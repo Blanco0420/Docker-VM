90d93d811ac427f425d7d2f2ff87e05cfd6a92aa
Blanco0420 <sachajamesterry@gmail.com>
2024-11-28 05:21:36 +0900

    Initial commit

--- hosts/master-1.nix
+++ hosts/master-1.nix
@@ -7,7 +7,10 @@
       address = "10.1.2.2";
       prefixLength = 24;
     }];
-    ipv4.gateway = "10.1.2.1";
+    defaultGateway = {
+      address = "10.1.2.1";
+      interface = "eth0";
+    };
   };
 
   networking.hostName = lib.mkForce "docker-master";
