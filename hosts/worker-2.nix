80fcf97abf76047fc8d7b32b702687dd4d6b01bb
Blanco0420 <sachajamesterry@gmail.com>
2024-11-28 05:52:03 +0900

    Initial commit

--- hosts/worker-2.nix
+++ hosts/worker-2.nix
@@ -1,12 +1,17 @@
 { ... }: {
   imports = [ ../nixos/configuration.nix ];
 
-  networking.interfaces.ens18 = {
-    useDHCP = false;
-    ipv4.addresses = [{
-      address = "10.1.2.4";
-      prefixLength = 24;
-    }];
-    ipv4.gateway = "10.1.2.1";
+  networking = {
+    interfaces.ens18 = {
+      useDHCP = false;
+      ipv4.addresses = [{
+        address = "10.1.2.4";
+        prefixLength = 24;
+      }];
+    };
+    defaultGateway = {
+      address = "10.1.2.1";
+      interface = "ens18";
+    };
   };
 }
