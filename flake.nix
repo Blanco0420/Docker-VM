9f1edd2a3067f5992a6616e19613a60155fd8a60
Blanco0420 <sachajamesterry@gmail.com>
2024-11-29 01:42:09 +0900

    Fixed stuff

--- flake.nix
+++ flake.nix
@@ -4,10 +4,14 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    comin = {
+      url = "github:nlewo/comin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, comin, ... }@inputs:
     let inherit (self) outputs;
     in {
       # NixOS configuration entrypoint
@@ -17,22 +21,78 @@
         docker-master1 = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
           # > Our main nixos configuration file <
-          modules = [ ./nixos/configuration.nix ./hosts/master-1.nix ];
+          modules = [
+            ./nixos/configuration.nix
+            ./hosts/master-1.nix
+            comin.nixosModules.comin
+            ({ ... }: {
+              services.comin = {
+                enable = true;
+                remotes = [{
+                  name = "origin";
+                  url = "https://github.com/Blanco0420/Docker-VM.git";
+                  branches.main.name = "main";
+                }];
+              };
+            })
+          ];
         };
         docker-worker1 = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
           # > Our main nixos configuration file <
-          modules = [ ./nixos/configuration.nix ./hosts/worker-1.nix ];
+          modules = [
+            ./nixos/configuration.nix
+            ./hosts/worker-1.nix
+            comin.nixosModules.comin
+            ({ ... }: {
+              services.comin = {
+                enable = true;
+                remotes = [{
+                  name = "origin";
+                  url = "https://github.com/Blanco0420/Docker-VM.git";
+                  branches.main.name = "main";
+                }];
+              };
+            })
+          ];
         };
         docker-worker2 = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
           # > Our main nixos configuration file <
-          modules = [ ./nixos/configuration.nix ./hosts/worker-2.nix ];
+          modules = [
+            ./nixos/configuration.nix
+            ./hosts/worker-2.nix
+            comin.nixosModules.comin
+            ({ ... }: {
+              services.comin = {
+                enable = true;
+                remotes = [{
+                  name = "origin";
+                  url = "https://github.com/Blanco0420/Docker-VM.git";
+                  branches.main.name = "main";
+                }];
+              };
+            })
+          ];
         };
         docker-worker3 = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
           # > Our main nixos configuration file <
-          modules = [ ./nixos/configuration.nix ./hosts/worker-3.nix ];
+          modules = [
+            ./nixos/configuration.nix
+            ./hosts/worker-3.nix
+            comin.nixosModules.comin
+            ({ ... }: {
+              services.comin = {
+                enable = true;
+                remotes = [{
+                  name = "origin";
+                  url = "https://github.com/Blanco0420/Docker-VM.git";
+                  branches.main.name = "main";
+                }];
+              };
+            })
+          ];
         };
 
       };
