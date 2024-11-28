e4d38de751c35c2c6a8b183a07cec9c112ccd35e
Blanco0420 <sachajamesterry@gmail.com>
2024-11-29 02:35:56 +0900

    Fixed stuff

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -19,7 +19,12 @@
   fileSystems."/mnt/shared" = {
     device = "u421299-sub4.your-storagebox.de/u421299-sub4";
     fsType = "cifs";
-    options = [ "credentials=/home/docker/Docker-VM/cifs_credentials" ];
+    options = let
+      # this line prevents hanging on network split
+      automount_opts =
+        "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
   };
 
   swapDevices = [ ];
