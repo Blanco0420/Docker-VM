dbe2eb58a587029befa34eadc6430c0816e51f71
Blanco0420 <sachajamesterry@gmail.com>
2024-12-03 18:02:31 +0900

    fixed mount permissions

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -1,8 +1,11 @@
 { config, lib, pkgs, modulesPath, ... }:
-let mountops = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+let
+  mountops =
+    "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 in {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules =
+    [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
@@ -13,14 +16,18 @@ in {
   fileSystems."/mnt/external" = {
     device = "//u421299-sub4.your-storagebox.de/u421299-sub4";
     fsType = "cifs";
-    options = ["${mountops},credentials=${config.age.secrets.external-smb.path}"];
+    options = [
+      "${mountops},credentials=${config.age.secrets.external-smb.path},uid=${config.users.users.docker.uid}"
+    ];
   };
   fileSystems."/mnt/local" = {
     device = "//10.1.2.120/storage";
     fsType = "cifs";
-    options = ["${mountops},credentials=${config.age.secrets.local-smb.path}"];
+    options = [
+      "${mountops},credentials=${config.age.secrets.local-smb.path},uid=${config.users.users.docker.uid}"
+    ];
   };
-swapDevices = [ ];
-networking.useDHCP = lib.mkDefault true;
-nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  swapDevices = [ ];
+  networking.useDHCP = lib.mkDefault true;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
