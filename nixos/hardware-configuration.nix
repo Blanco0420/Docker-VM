afa268b9552546b41ff6845ff219c4176331c4b6
Blanco0420 <sachajamesterry@gmail.com>
2024-12-03 13:57:46 +0900

    fixed error

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -3,7 +3,8 @@
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules =
+    [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
@@ -14,27 +15,24 @@
   fileSystems."/mnt/local" = {
     device = "//10.1.2.120/storage";
     fsType = "cifs";
-    options =
-      let
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      in
-      [
-        "${automount_opts},credentials=config.${config.age.secrets.local-smb.path},uid=1001,gid=131"
-      ];
+    options = let
+      automount_opts =
+        "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in [
+      "${automount_opts},credentials=config.${config.age.secrets.local-smb.path},uid=1001,gid=131"
+    ];
   };
   fileSystems."/mnt/external" = {
     device = "//u421299-sub4.your-storagebox.de/u421299-sub4";
     fsType = "cifs";
-    options =
-      let
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      in
-      [
-        "${automount_opts},credentials=config.${config.age.secrets.external-smb.path},uid=1001,gid=131"
-      ];
+    options = let
+      automount_opts =
+        "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in [
+      "${automount_opts},credentials=config.${config.age.secrets.external-smb.path},uid=1001,gid=131"
+    ];
   };
-};
-swapDevices = [ ];
-networking.useDHCP = lib.mkDefault true;
-nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  swapDevices = [ ];
+  networking.useDHCP = lib.mkDefault true;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
