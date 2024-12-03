063640e230c3810fa77968c2670b6de3b1800736
Blanco0420 <sachajamesterry@gmail.com>
2024-12-03 14:30:38 +0900

    fixed error

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -12,17 +12,17 @@
     device = "/dev/sda1";
     fsType = "ext4";
   };
-  fileSystems."/mnt/local" = {
-    device = "//10.1.2.120/storage";
-    fsType = "cifs";
-    options = let
-      secretPath = config.age.secrets.local-smb.path;
-      automount_opts =
-        "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in [
-      "${automount_opts},credentials=${config.age.secrets.local-smb.path},uid=${config.users.users.docker.uid}"
-    ];
-  };
+  # fileSystems."/mnt/local" = {
+  #   device = "//10.1.2.120/storage";
+  #   fsType = "cifs";
+  #   options = let
+  #     secretPath = config.age.secrets.local-smb.path;
+  #     automount_opts =
+  #       "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+  #   in [
+  #     "${automount_opts},credentials=${config.age.secrets.local-smb.path},uid=${config.users.users.docker.uid}"
+  #   ];
+  # };
   fileSystems."/mnt/external" = {
     device = "//u421299-sub4.your-storagebox.de/u421299-sub4";
     fsType = "cifs";
@@ -31,7 +31,7 @@
       automount_opts =
         "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
     in [
-      "${automount_opts},credentials=${config.age.secrets.external-smb.path},uid=${config.users.users.docker.uid}"
+      "${automount_opts},credentials=${config.age.secrets.external-smb.path}"
     ];
   };
   swapDevices = [ ];
