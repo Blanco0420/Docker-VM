79c0ae95a779e2d3645fe3de06b2bc0620c21649
Workstation <github@mylesbolton.com>
2024-12-05 00:03:56 +0000

    asdasad

--- hosts/worker-3.nix
+++ hosts/worker-3.nix
@@ -10,5 +10,13 @@
       }];
     };
   };
+    age.secrets = {
+    local-smb.file = ../secrets/local-smb.age;
+    external-smb.file = ../secrets/external-smb.age;
+    docker-ssh-key.file = ../secrets/docker-ssh-key.age;
+    glusterca.file = ../secrets/glusterca.age;
+    glusterpem.file = ../secrets/glusterpem-docker-worker-3.age;
+    glusterkey.file = ../secrets/glusterkey-docker-worker-3.age;
+      };
   networking.hostName = lib.mkForce "docker-worker-3";
 }
