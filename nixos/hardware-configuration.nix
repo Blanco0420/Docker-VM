1f6e64b71a7189d93b34ae6a482838a1ff7edef3
Workstation <github@mylesbolton.com>
2024-12-05 01:22:36 +0000

    hgfh

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -5,10 +5,6 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
-  fileSystems."/" = {
-    device = "/dev/sda";
-    fsType = "btrfs";
-  };
   swapDevices = [ ];
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
