d4f893f39d5b7ed75e2ba3a34eb0edb8befa639b
Workstation <github@mylesbolton.com>
2024-12-06 09:57:12 +0000

    networking hosts

--- nixos/system/networking.nix
+++ nixos/system/networking.nix
@@ -9,5 +9,14 @@
         [ 22 2376 2377 7946 4789 ]; # Allow SSH and Docker Swarm ports
       allowedUDPPorts = [ 7946 4789 ]; # Allow Docker Swarm UDP ports
     };
+    extraHosts =
+    ''
+      10.1.2.11 docker-master-1
+      10.1.2.12 docker-master-2
+      10.1.2.101 docker-worker-1
+      10.1.2.102 docker-worker-2
+      10.1.2.103 docker-worker-3
+      10.1.2.104 docker-worker-4
+    '';
   };
 }
