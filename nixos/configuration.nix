358bf01f9f25ab520612344e7d0eb799e01ae791
Workstation <github@mylesbolton.com>
2024-12-07 00:12:37 +0000

    remove old calls to creds and fixed sshfs mount

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -50,8 +50,6 @@
   ];
 
   age.secrets = {
-    local-smb.file = ../secrets/local-smb.age;
-    external-smb.file = ../secrets/external-smb.age;
     docker-ssh-key.file = ../secrets/docker-ssh-key.age;
     glusterca.file = ../secrets/glusterca.age;
   };
