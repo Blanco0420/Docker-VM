a444fa621162f11bcb96b3ee78e127b5ac5002a2
Workstation <github@mylesbolton.com>
2024-12-16 09:40:56 +0000

    remove mount ops from gluster

--- nixos/system/mounts.nix
+++ nixos/system/mounts.nix
@@ -1,5 +1,5 @@
 let 
-  mountops = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
+  cifsmountops = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
 in 
 { config, ... }: {
 
@@ -8,13 +8,13 @@ in
       device = "//u421299-sub4.your-storagebox.de/u421299-sub4";
       mountPoint = "/mnt/external";
       fsType = "cifs";
-      options = [ "forceuid,forcegid,${mountops},uid=${toString config.users.users.docker.uid},gid=${toString config.users.groups.docker.gid},credentials=${config.age.secrets.external-smb.path}" ];
+      options = [ "forceuid,forcegid,${cifsmountops},uid=${toString config.users.users.docker.uid},gid=${toString config.users.groups.docker.gid},credentials=${config.age.secrets.external-smb.path}" ];
     };
     gluster = {
       device = "localhost:/persist/glustervol";
       mountPoint = "/mnt/gluster";
       fsType = "glusterfs";
-      options = [ "${mountops},uid=${toString config.users.users.docker.uid},gid=${toString config.users.groups.docker.gid}" ];
+      options = [ "uid=${toString config.users.users.docker.uid},gid=${toString config.users.groups.docker.gid}" ];
     };
   };
 }
