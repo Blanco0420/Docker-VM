b1462ee020d83e089063d90e09b19fe66a78c98c
Workstation <github@mylesbolton.com>
2024-12-16 08:39:32 +0000

    removed force uid and gid from mount ops and added them to external cifs only

--- nixos/system/mounts.nix
+++ nixos/system/mounts.nix
@@ -1,5 +1,5 @@
 let 
-  mountops = "forceuid,forcegid,x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
+  mountops = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
 in 
 { config, ... }: {
 
@@ -8,7 +8,7 @@ in
       device = "//u421299-sub4.your-storagebox.de/u421299-sub4";
       mountPoint = "/mnt/external";
       fsType = "cifs";
-      options = [ "${mountops},uid=${toString config.users.users.docker.uid},gid=${toString config.users.groups.docker.gid},credentials=${config.age.secrets.external-smb.path}" ];
+      options = [ "forceuid,forcegid,${mountops},uid=${toString config.users.users.docker.uid},gid=${toString config.users.groups.docker.gid},credentials=${config.age.secrets.external-smb.path}" ];
     };
     gluster = {
       device = "/persist/glustervol";
